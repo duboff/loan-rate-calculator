c83d666a09d88cf78d3aa6de49e3126bae87a7ef
Mikhail Dubov <mdubov@gmail.com>
2014-05-31 14:29:41 +0100

    Added rate calculation for simple cases

--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,1 +1,22 @@
 #!/usr/bin/ruby
+
+class Calculator
+
+  attr_accessor :rate, :initial_amount, :lender_proposals
+
+  def get_rate
+    sorted_proposals = self.lender_proposals.sort_by {|prop| prop[1] }
+
+    rates = sorted_proposals.map {|prop| prop[1] }
+    amts = sorted_proposals.map {|prop| prop[2] }
+    result = 0
+    amount = initial_amount
+    rates.each_with_index do |cur_rate, i|
+      a = [amts[i], amount].min
+      result += cur_rate * a
+      amount -= a if amount > 0
+    end
+    self.rate = (result / self.initial_amount).round(2)
+  end
+
+end
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,2 +1,17 @@
 require 'rspec'
 require 'calculator'
+
+describe Calculator do
+  it 'calculates selects correct rate' do
+    calc = Calculator.new
+    calc.initial_amount = 1000
+    calc.lender_proposals = [
+      ['Bob', 0.075, 640],
+      ['Jane', 0.069, 480],
+      ['Fred', 0.071, 520],
+      ['Mary', 0.104, 170],
+    ]
+    calc.get_rate
+    expect(calc.rate).to eq 0.07
+  end
+end
